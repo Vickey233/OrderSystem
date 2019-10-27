9acfcd0be22e0a52e00d70bcc8f06c6b6628a52e
vickey <1446792765@qq.com>
2019-10-27 19:43:26 +0800

    1

--- src/main/java/com/groupthree/ordersystem/controller/KaptchaController.java
+++ src/main/java/com/groupthree/ordersystem/controller/KaptchaController.java
@@ -1,37 +1,36 @@
 package com.groupthree.ordersystem.controller;
 
-import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.groupthree.ordersystem.aop.WebLog;
-import com.groupthree.ordersystem.utils.ResultUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 
-@RestController
+import com.groupthree.ordersystem.aop.WebLog;
+import com.groupthree.ordersystem.utils.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+
+@Slf4j
+@Controller
 public class KaptchaController {
+
     /**
      * 1、验证码工具
      */
     @Autowired
     DefaultKaptcha defaultKaptcha;
-    /**
-     * 2、生成验证码
-     *
-     * @param httpServletRequest
-     * @param httpServletResponse
-     * @throws Exception
-     */
-    @WebLog(description = "生成验证码")
+
+//    @WebLog(description = "生成验证码")
     @RequestMapping("/defaultKaptcha")
     public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws Exception {
@@ -46,7 +45,9 @@ public class KaptchaController {
             ImageIO.write(challenge, "jpg", jpegOutputStream);
         } catch (IllegalArgumentException e) {
             httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return;}
+            return;
+        }
+
         // 定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
         captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
         httpServletResponse.setHeader("Cache-Control", "no-store");
@@ -57,20 +58,26 @@ public class KaptchaController {
         responseOutputStream.write(captchaChallengeAsJpeg);
         responseOutputStream.flush();
         responseOutputStream.close();
+        jpegOutputStream.close();
     }
+
 
     @WebLog(description = "验证，验证码")
     @RequestMapping("/checkCode")
+    @ResponseBody
     public Object checkCode(HttpServletRequest httpServletRequest,
-                                                   @RequestParam("code") String code) {
+                            @RequestParam("code") String code) {
         String rightCode = (String) httpServletRequest.getSession().getAttribute("rightCode");
         String ClientCode = code;
 //        rightCode是生成码，ClientCode是表单提交码
         System.out.println("rightCode:" + rightCode + " ———— ClientCode:" + ClientCode);
+        log.info("验证,验证码");
         if (!rightCode.equals(ClientCode)) {
             return ResultUtil.error("验证码错误");
         } else {
             return ResultUtil.successTip("登录成功");
         }
     }
+
 }
+
